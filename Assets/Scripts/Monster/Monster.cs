b25e97e5ef23905791871f436603bf2d8a39a50d
jseol0 <79302243+jseol0@users.noreply.github.com>
2025-03-17 22:16:21 +0900

    temp: 몬스터 부모 클래스 임시 구현
    hp, moveSpeed, attack
    Move()
    Attack()
    Damaged()
    Die()

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
 
+//부모클래스 임시설정
 public class Monster : MonoBehaviour
 {
-    public float HP = 100f;
-    void Start()
+    public float hp = 100f;
+    public float attack = 1f;
+    public float moveSpeed = 1f;
+
+    protected virtual void Move()
+    { }
+
+    protected virtual void Attack()
+    { }
+
+    protected virtual void Damaged(float damage)
     {
-        
+        hp -= damage;
+
+        if (hp <= 0)
+            Die();
     }
 
-    void Update()
+    protected virtual void Die()
     {
-        
+        Destroy(gameObject);
     }
 }
