a812f780dd8d6bb317a47b7aa200bf1a593651bf
JunwonGame <junwon9913@gmail.com>
2025-03-26 14:04:48 +0900

    Boss_0326
    800:600 해상도에 맞춘
    보스 스프라이트 크기 재조정

--- Assets/Scripts/Boss/HatchingUFO.cs
+++ Assets/Scripts/Boss/HatchingUFO.cs
@@ -11,6 +11,6 @@ public class HatchingUFO : MonoBehaviour
 
     void SpawnUFO() //Hatching UFO 애니메이션 창의 맨 끝 프레임에서 호출할 함수
     {
-        GameObject miniUFO = Instantiate(MiniUFOPrefab, new Vector3(bossTransform.position.x - 0.008f, bossTransform.position.y - 1.138f, 0), Quaternion.identity); //다이만지 기준 위치 설정
+        GameObject miniUFO = Instantiate(MiniUFOPrefab, new Vector3(bossTransform.position.x - 0.177f, bossTransform.position.y - 3.531f, 0), Quaternion.identity); //다이만지 기준 위치 설정
     }
 }
